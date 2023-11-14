d24236195b75707ecfff35fefe1020c53825366e
Castellano46 <pedroliebana35@gmail.com>
2023-11-14 09:12:21 +0100

    first commit

---
+++ SuperPoderes/SuperPoderesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperPoderesApp.swift
+//  SuperPoderes
+//
+//  Created by Pedro on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperPoderesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
